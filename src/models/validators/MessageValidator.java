17c6e196ff949b77dead7b9d85af3ba2ff04ef25
reo.murata <lavi.mlty@gmail.com>
2019-11-25 00:15:34 +0900

    Add validation

---
+++ src/models/validators/MessageValidator.java
@@ -0,0 +1,28 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Message;
+
+public class MessageValidator {
+
+    public static List<String> validate(Message m) {
+        List<String> errors = new ArrayList<String>();
+
+        String content_error = _validateContent(m.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        return errors;
+    }
+
+    private static String _validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "タスク内容を入力してください。";
+        }
+
+        return "";
+    }
+}
